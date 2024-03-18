07ad2b57605fa1ffc60cbee89de6faa8aba39ff4
Diêgo Carvalho <157425806+diegocrvo@users.noreply.github.com>
2024-03-18 20:51:29 -0300

    Resolução do exercício 081 de Python

--- PythonExercicios/PythonExercicios03/ex081.py
+++ PythonExercicios/PythonExercicios03/ex081.py
@@ -3,3 +3,17 @@ Depois disso, mostre:
 A) Quantos números foram digitados.
 B) A lista de valores, ordenada de forma decrescente.
 C) Se o valor 5 foi digitado e está ou não na lista."""
+
+list = list()
+while True:
+    list.append(int(input('Digite um número: ')))
+    comand = str(input('Deseja continuar?[S/N]: '))
+    if comand in 'Nn':
+        break
+list.sort(reverse=True)
+print(f'Foram digitados {len(list)} números.')
+print(f'Os valores digitados em ordem decrescente foram {list}')
+if 5 in list:
+    print('O valor 5 se encontra na lista.')
+else:
+    print('O valor 5 não se encontra na lista.')
