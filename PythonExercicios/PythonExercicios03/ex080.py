ec5e023d3de1b013c701a2e61b1a301f993cb083
Diêgo Carvalho <157425806+diegocrvo@users.noreply.github.com>
2024-03-18 20:50:41 -0300

    Resolução do exercício 080 de Python

--- PythonExercicios/PythonExercicios03/ex080.py
+++ PythonExercicios/PythonExercicios03/ex080.py
@@ -2,3 +2,16 @@
 em uma lista, já na posição correta de inserção (sem usar o sort()).
 No final, mostre a lista ordenada na tela."""
 
+list = []
+for c in range(0, 5):
+    value = (int(input('Digite um número: ')))
+    if c == 0 or value > list[-1]:
+        list.append(value)
+    else:
+        pos = 0
+        while pos < len(list):
+            if value <= list[pos]:
+                list.insert(pos, value)
+                break
+            pos += 1
+print(f'Os valores digitados em ordem foram {list}.')
