0263c6d06d75204c736aab8ab0b82b216a546104
Diêgo Carvalho <157425806+diegocrvo@users.noreply.github.com>
2024-03-18 19:47:15 -0300

    Resolução do exercício 079 de Python

--- PythonExercicios/PythonExercicios03/ex079.py
+++ PythonExercicios/PythonExercicios03/ex079.py
@@ -1,3 +1,14 @@
 """Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os
 em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, será
 exibido todos os valores únicos digitados, em ordem crescente."""
+
+values = list()
+while True:
+    values.append(int(input('Digite um valor: ')))
+    if values[-1] in values[:-2]:
+        del(values[-1])
+        print('O valor foi duplicado, não será adicionado...')
+    comando = str(input('Deseja continuar?[S/N]: '))
+    if comando in 'nN':
+        break
+print(f'Você digitou os valores {sorted(values)}.')
