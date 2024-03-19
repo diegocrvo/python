9b091ac495a0fc06c8a8db17e27d58ad83b6f8bb
Diêgo Carvalho <157425806+diegocrvo@users.noreply.github.com>
2024-03-19 20:42:02 -0300

    Resolução do exercício 082 de Python

--- PythonExercicios/PythonExercicios03/ex082.py
+++ PythonExercicios/PythonExercicios03/ex082.py
@@ -2,3 +2,20 @@
 Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados,
 respectivamente.
 Ao final, mostre o conteúdo das três listas geradas."""
+
+list = []
+listpar = []
+listimpar = []
+while True:
+    value = int(input('Digite um número: '))
+    list.append(value)
+    comand = str(input('Deseja continuar?[S/N]: '))
+    if value % 2 == 0:
+        listpar.append(value)
+    else:
+        listimpar.append(value)
+    if comand in 'Nn':
+        break
+print(f'Os valores digitados foram {list}')
+print(f'Os valores pares digitados foram {listpar}')
+print(f'Os valores ìmpares digitados fooram {listimpar}')
