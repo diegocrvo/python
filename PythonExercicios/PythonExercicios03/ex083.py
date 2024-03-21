f61af5da9e69742578fba789f53ca686b1df6011
Diêgo Carvalho <157425806+diegocrvo@users.noreply.github.com>
2024-03-20 21:46:04 -0300

    Resolução do exercício 083 de Python

--- PythonExercicios/PythonExercicios03/ex083.py
+++ PythonExercicios/PythonExercicios03/ex083.py
@@ -1,2 +1,21 @@
 """Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo
 deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta."""
+
+open = close = 0
+list = list()
+while True:
+    value = str(input('Digite a expressão: '))
+    for simbol in value:
+        if simbol == '(':
+            open += 1
+        if simbol == ')':
+            close += 1
+    if open == close:
+        print(f'A expresão é válida.')
+    else:
+        print(f'A expresão não é válida.')
+    open = close= 0
+    comand = str(input('Deseja continuar?[S/N]: ')).strip()[0]
+    if comand in 'Nn':
+        break
+print('\033[31mPrograma Encerrado.\033[m')
