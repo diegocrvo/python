9863dcb27fe68ac756034602160f3debd8495084
Diêgo Carvalho <157425806+diegocrvo@users.noreply.github.com>
2024-04-29 22:24:47 -0300

    Resolução exercício 084 de Python

--- PythonExercicios/PythonExercicios03/ex083.py
+++ PythonExercicios/PythonExercicios03/ex083.py
@@ -1,9 +1,9 @@
 """Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo
 deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta."""
 
-exp = str(input('Digite a expresão: '))
+expression = str(input('Digite a expresão: '))
 list = []
-for simb in exp:
+for simb in expression:
     if simb == '(':
         list.append('(')
     elif simb == ')':
--- PythonExercicios/PythonExercicios03/ex084.py
+++ PythonExercicios/PythonExercicios03/ex084.py
@@ -2,3 +2,32 @@
 A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves."""
+
+temp = []
+principal = []
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(str(input('Peso: ')))
+    if len(principal) == 0:
+        menor = maior = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    principal.append(temp[:])
+    temp.clear()
+    comando = str(input('Deseja continuar? [S/N]: '))
+    if comando in 'Nn':
+        break
+print(principal)
+print(f'Foram cadastradas {len(principal)} pessoas.')
+print(f'O maior peso foi de {maior}Kg. Peso de ', end='')
+for p in principal:
+    if p[1] == maior:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi de {menor}Kg. Peso de ', end='')
+for p in principal:
+    if p[1] == menor:
+        print(f'[{p[0]}] ', end='')
